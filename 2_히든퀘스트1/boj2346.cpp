490cda421c3cbb7016a956f990338a2094a42ba3
SungwookLE <joker1251@naver.com>
2024-03-08 00:43:15 +0900

    hiddenQuest DOne

--- 2_히든퀘스트1/boj2346.cpp
+++ 2_히든퀘스트1/boj2346.cpp
@@ -3,18 +3,45 @@
 using namespace std;
 
 int N;
-vector<pair<int, int>> q;
+struct node{
+    int id, v;
+    node* prev_;
+    node* next_;
+} NODE[1002];
 
 int main(){
     cin >> N;
-    q = vector<pair<int, int>>(N);
 
-    for(int i =0 ; i < N; ++i){
-        q[i].first = i+1;   // idx
-        cin >> q[i].second; // value
+    int n;
+    for(int i =1 ; i <= N; ++i){
+        cin >> n;
+        NODE[i].v = n;
+        NODE[i].id = i;
+        NODE[i].prev_ = &NODE[i-1];
+        NODE[i].next_ = &NODE[i+1];
     } 
+    NODE[1].prev_ = &NODE[N];
+    NODE[N].next_ = &NODE[1];
 
+    node* cur = &NODE[1];
 
+    while(1){
+        cout << cur->id << " ";
+        cur->next_->prev_ = cur->prev_; //실제로 삭제하지 않고도 삭제의 효과 (시간복잡도1)
+        cur->prev_->next_ = cur->next_; //실제로 삭제하지 않고도 삭제의 효과 (시간복잡도1)
+
+        if (cur->next_ == cur || cur->prev_ == cur) break;
+
+        int mov = cur->v;
+
+        if (mov > 0){
+            while(mov--) cur = cur->next_;
+        }
+        else if (mov <0 ){
+            while(mov++) cur = cur->prev_;
+        }
+    }
+    
    
     return 0;
 }
--- 2_히든퀘스트1/boj24511.cpp
+++ 2_히든퀘스트1/boj24511.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int N, M;
 vector<int> A;
 vector<int> B;
+queue<int> Q;
 int c, ret;
 
 int main(){
@@ -18,25 +19,18 @@ int main(){
 
     for(int i =0 ; i < N; ++i) cin >> A[i];
     for(int i =0 ; i < N; ++i) cin >> B[i];
-
-    for(int i = N-1 ; i >= 0 ; --i){
-        if (A[i] == 1){
-            A.erase(A.begin()+i);
-            B.erase(B.begin()+i);
-        }
-    }
+    for(int i = N-1 ; i >= 0 ; --i)
+        if (A[i] == 0) Q.push(B[i]);
 
     cin >> M;
-    for(int i =0 ; i < M; ++i){
-        cin >> c; ret = c;
-        for(int j = 0 ; j < A.size() ; ++j){
-            int temp = ret;
-            ret = B[j];
-            B[j] = temp;
-        }
+    int q_id = 0;
+    for(int i = 0 ; i < M ; ++i){
+        cin >> c;
+        Q.push(c);
+        cout << Q.front() << " ";
+        Q.pop();
+    }   
 
-        cout << ret << " ";
-    } 
 
     return 0;
 }
